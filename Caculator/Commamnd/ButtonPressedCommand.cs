0767a00310760415e66ee13703579a210e6753d3
PntNoobDev <phamngocthanh.d21ctc1@muce.edu.vn>
2024-07-09 19:59:39 +0700

    Add project files.

---
+++ Caculator/Commamnd/ButtonPressedCommand.cs
@@ -0,0 +1,39 @@
+﻿using Calculator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator.Commands
+{
+    internal class ButtonPressedCommand : ICommand
+    {
+        private MainViewModel _viewModel;
+
+        public MainViewModel viewModel
+        {
+            get { return _viewModel; }
+            set { _viewModel = value; }
+        }
+
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object button)
+        {
+            viewModel.GetPressedButton(button.ToString());
+        }
+
+        public ButtonPressedCommand(MainViewModel mainViewModel)
+        {
+            viewModel = mainViewModel;
+        }
+    }
+}
---
+++ Caculator/Models/ArithmeticOperators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    internal class ArithmeticOperators
+    {
+        public bool Addition { get; set; }
+        public bool Subtraction { get; set; }
+        public bool Division { get; set; }
+        public bool Multiplication { get; set; }
+
+        public ArithmeticOperators()
+        {
+            Addition = false;
+            Subtraction = false;
+            Division = false;
+            Multiplication = false;
+        }   
+    }
+}
